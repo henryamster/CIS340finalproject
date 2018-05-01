b7e829aa0e2b71299670a9d46206cb537ae5001b
Henry Fritz <henryamsterfritz@gmail.com>
2018-05-01 03:53:51 -0400

    style changes

--- public/viewPosts.php
+++ public/viewPosts.php
@@ -1,4 +1,23 @@
 
 <?php include('../includes/header.php'); ?>
+<div class="container">
 
+    <div class="card white-bg listing">
+
+        <h4 class="course">MKT300 </h4> 
+        <h3 class="title"> Principles of Marketing</h3>
+        <p>Post content Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut pariatur totam repellat dolorum magnam quaerat nihil reprehenderit perspiciatis doloribus nulla mollitia error nostrum inventore, ducimus aspernatur ipsum ex, harum doloremque!</p>
+        <p>email: <a href="mailto:test@example.com">test@example.com</a></p>
+    
+    </div>
+    <hr>
+    <div class="card white-bg listing">
+
+        <h4 class="course">CIS 340 </h4> 
+        <h3 class="title"> XML, HTML, PHP</h3>
+        <p>Post content Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut pariatur totam repellat dolorum magnam quaerat nihil reprehenderit perspiciatis doloribus nulla mollitia error nostrum inventore, ducimus aspernatur ipsum ex, harum doloremque!</p>
+        <p>email: <a href="mailto:test@example.com">test@example.com</a></p>
+    
+    </div>
+</div>
 <?php include('../includes/footer.php'); ?>
